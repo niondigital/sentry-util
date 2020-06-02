a533c6d744bd3c9d1a1a8ad9fd2736d9173b77b7
David Seibert <david@seibert.io>
2020-06-02 16:00:05 +0200

    refactor: use linting-base package

---
+++ .huskyrc.js
@@ -0,0 +1,1 @@
+module.exports = require('@madebyheyday/linting-base/husky.config')
--- commitlint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	extends: ['@commitlint/config-conventional', 'cz'],
-	rules: {
-		'header-max-length': [2, 'always', 82]
-	}
-};
